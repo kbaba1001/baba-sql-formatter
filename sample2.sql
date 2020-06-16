605b3ef14c16c904bbd94097194e5ef91dc06cd7
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-06-16 21:53:35 +0900

    add sample2.sql

--- sample2.sql
+++ sample2.sql
@@ -1,40 +1,7 @@
-DELETE
-FROM
-    table1
-WHERE
-    col1 = 'aaa'
-    AND col2 = 'bbb'
-    AND TRUNC(created_at) = DATE_TRUNC('week', CONVERT_TIMEZONE('JST', GETDATE()))
-;
+DELETE FROM table1 WHERE col1 = 'aaa' AND col2 = 'bbb' AND TRUNC(created_at) = DATE_TRUNC('week', CONVERT_TIMEZONE('JST', GETDATE()));
 
-INSERT INTO table1
-WITH
-table2 AS(
-     SELECT
-         id
-        ,MAX(col1) AS max_col1
-    FROM
-        table3
-    GROUP BY
-         id
-)
-,xxx AS(
-    SELECT
-        col1
-        ,col2
-        ,col3
-    FROM
-        table2
-)
-SELECT
-     id
-    ,DATE_TRUNC('year', col1) AS year
-    ,DATEADD('day', - 365, created_at) AS begin_date
-FROM
-    xxx
-    LEFT JOIN yyy AS y
-      ON xxx.id = y.xxx_id
-;
+INSERT INTO table1 WITH
+table2 AS( SELECT id ,MAX(col1) AS max_col1 FROM table3 GROUP BY id) ,xxx AS(SELECT col1 ,col2 ,col3 FROM table2)
+SELECT id ,DATE_TRUNC('year', col1) AS year ,DATEADD('day', -365, created_at) AS begin_date FROM xxx LEFT JOIN yyy AS y ON xxx.id = y.xxx_id;
 
-COMMIT
-;
+COMMIT;
